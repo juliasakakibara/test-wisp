9516aec8218e9f25221e8cec23e72c06b3bb9080
Julia Sakakibara <juliasakakibara.o@gmail.com>
2026-03-07 04:45:55 -0300

    Update default Wisp Blog ID fallback

--- src/lib/wisp.ts
+++ src/lib/wisp.ts
@@ -1,6 +1,6 @@
 import { buildWispClient } from "@wisp-cms/client";
 
-const blogId = process.env.NEXT_PUBLIC_WISP_BLOG_ID || "clvztruzw0000a6f0iiz5ng2c"; // Default to wisp demo blog if not set
+const blogId = process.env.NEXT_PUBLIC_WISP_BLOG_ID || "8cfca777-6eb1-4609-b05a-1ca12ace047d";
 
 export const wisp = buildWispClient({
     blogId,
