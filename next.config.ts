62370f88ef5e2023f049c104ddefdbe911660912
Julia Sakakibara <juliasakakibara.o@gmail.com>
2026-03-07 05:44:23 -0300

    Migrate deployment to GitLab CI

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/test-wisp",
+  basePath: "/julia1oliveira.26-project",
   images: {
     unoptimized: true,
   },
