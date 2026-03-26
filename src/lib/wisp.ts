736b9b970ff61e1f0c7905928d6a205fe0df9a0b
Julia Sakakibara <juliasakakibara.o@gmail.com>
2026-03-26 11:14:47 -0300

    chore: update wisp blog id

--- src/lib/wisp.ts
+++ src/lib/wisp.ts
@@ -1,6 +1,6 @@
 import { buildWispClient } from "@wisp-cms/client";
 
-const blogId = process.env.NEXT_PUBLIC_WISP_BLOG_ID || "8cfca777-6eb1-4609-b05a-1ca12ace047d";
+const blogId = process.env.NEXT_PUBLIC_WISP_BLOG_ID || "bb96a9b8-aad3-4284-a042-43c77821df78";
 
 export const wisp = buildWispClient({
     blogId,
